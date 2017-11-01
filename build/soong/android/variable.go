a0b6cd75e79136dd2c7277947be29b72823a7391
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-31 20:01:35 -0700

    candy: Add TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY variable handling
    Change-Id: I6f1cbcd883e91c8c3724dbe53732c7e777e21657

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,20 +1,15 @@
 package android
 type Product_variables struct {
-	Has_legacy_camera_hal1 struct {
-		Cflags []string
-	}
-
-	Uses_media_extensions struct {
-		Cflags []string
-	}
-
-	Needs_text_relocations struct {
+    Needs_text_relocations struct {
 		Cppflags []string
+	}
+	Uses_generic_camera_parameter_library struct {
+		Srcs []string
 	}
 }
 
 type ProductVariables struct {
-	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Uses_media_extensions   *bool `json:",omitempty"`
+	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
+	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Needs_text_relocations  *bool `json:",omitempty"`
 }

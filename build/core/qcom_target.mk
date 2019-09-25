7e880051505ff419cf0ca746e3033b6d16480618
Michael Bestas <mkbestas@lineageos.org>
2019-09-25 20:57:26 +0300

    build: Remove ability to set WLAN/BT_VENDOR variant
    * These flags have no users since cm-14.1
    
    Change-Id: Ia2b2fd5d95e856fdae3ec23532bc6eceff2910c6

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -1,10 +1,3 @@
-define wlan-set-path-variant
-$(call project-set-path-variant,wlan,TARGET_WLAN_VARIANT,hardware/$(1)/wlan)
-endef
-define bt-vendor-set-path-variant
-$(call project-set-path-variant,bt-vendor,TARGET_BT_VENDOR_VARIANT,hardware/$(1)/bt)
-endef
-
 # Set device-specific HALs into project pathmap
 define set-device-specific-path
 $(if $(USE_DEVICE_SPECIFIC_$(1)), \
@@ -17,13 +10,12 @@ endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
-$(call set-device-specific-path,PLATFORM_SUPERPROJECT,platform-superproject,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT))
-
 $(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
 $(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
 $(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
 
-$(call set-device-specific-path,CAMERA,camera,hardware/qcom-caf/camera)
+$(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom/bt-caf)
+$(call set-device-specific-path,CAMERA,camera,hardware/qcom/camera)
 $(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
 $(call set-device-specific-path,GPS,gps,hardware/qcom-caf/gps)
 $(call set-device-specific-path,SENSORS,sensors,hardware/qcom-caf/sensors)
@@ -32,9 +24,7 @@ $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource
 $(call set-device-specific-path,POWER,power,hardware/qcom-caf/power)
 $(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/thermal)
 $(call set-device-specific-path,VR,vr,hardware/qcom-caf/vr)
-
-$(call wlan-set-path-variant,qcom-caf)
-$(call bt-vendor-set-path-variant,qcom-caf)
+$(call set-device-specific-path,WLAN,wlan,hardware/qcom/wlan-caf)
 
 PRODUCT_CFI_INCLUDE_PATHS += \
     hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
@@ -44,6 +34,7 @@ $(call project-set-path,qcom-audio,hardware/qcom/audio)
 $(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM))
 $(call project-set-path,qcom-media,hardware/qcom/media/$(TARGET_BOARD_PLATFORM))
 
+$(call project-set-path,qcom-bt-vendor,hardware/qcom/bt)
 $(call project-set-path,qcom-camera,hardware/qcom/camera)
 $(call project-set-path,qcom-data-ipa-cfg-mgr,hardware/qcom/data/ipacfg-mgr)
 $(call project-set-path,qcom-gps,hardware/qcom/gps)
@@ -51,7 +42,6 @@ $(call project-set-path,qcom-sensors,hardware/qcom/sensors)
 $(call project-set-path,qcom-loc-api,vendor/qcom/opensource/location)
 $(call project-set-path,qcom-dataservices,$(TARGET_DEVICE_DIR)/dataservices)
 
-$(call wlan-set-path-variant,qcom)
-$(call bt-vendor-set-path-variant,qcom)
+$(call project-set-path,qcom-wlan,hardware/qcom/wlan)
 
 endif
